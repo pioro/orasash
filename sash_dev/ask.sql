6a0dd1a97a7ccb30c104bfacbe9787c7764fcd88
Kyle Hailey <kylelf@gmail.com>
2012-01-03 13:55:19 +0000

    version 1

--- sash_dev/ask.sql
+++
@@ -1,4 +0,0 @@
-
-accept HOST1 prompt "Enter hostname for instance number 1 "                                                                                                                                             
-accept HOST2 prompt "Enter hostname for instance number 2 "                                                                                                                                             
-
