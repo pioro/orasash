bbf2480cea26523578f8111a2a7dcd14964250ff
Marcin Przepiorowski <pioro1@gmail.com>
2012-01-04 09:11:37 +0000

    repository clean up. Moving old version to branches. This is version 2.3+

--- releases/v2.3-rc1/instance.sql
+++
@@ -1,4 +0,0 @@
-
-accept HOST1 prompt "Enter host name for instance number 1 "                                                                                                                                            
-
-accept INST1 default datag prompt "Enter instance name for instance number 1 [ default datag ] "                                                                                                        
