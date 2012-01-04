15a2536896597078d95b40bb264f06ef506081c9
Marcin Przepiorowski <pioro1@gmail.com>
2012-01-04 08:54:26 +0000

    Revert "Revert "Revert "10g fix, same db name fix"""
    This reverts commit 128bfeb552adbb71a24bc544f4d427f09de7de80.

--- sash_dev/adddb.sql
+++ sash_dev/adddb.sql
@@ -22,16 +22,12 @@ SET TERMOUT ON
 
 accept PORT default 1521 prompt "Enter listener port number [default 1521] "
 accept SASHPASS prompt "Enter SASH password on target database "
-accept VER default 11.2 prompt "Enter version of target database in following format 10.2,11.1,11.2 [default 11.2] "
 
--- set term off
-column f_st new_value v_dblink 
-select '&DBNAME' || '1' || replace(substr('&HOST1',1,8),'.','_') f_st from dual;
-
+set term off
 spool setup.sql
-select 'exec sash_repo.add_db(''&' || 'HOST' || rownum || ''', ' || &PORT || ', ''&SASHPASS'', ''&DBNAME'', ''&' || 'INST' || rownum || ''',' || rownum || ', ''' ||&VER ||''', 8);' from all_source where rownum <= &INST_NUM;
-select 'exec sash_pkg.configure_db(''&V_DBLINK'');' from dual;
-select 'exec sash_pkg.set_dbid(''&V_DBLINK'');' from dual;
+select 'exec sash_repo.add_db(''&' || 'HOST' || rownum || ''', ' || &PORT || ', ''&SASHPASS'', ''&DBNAME'', ''&' || 'INST' || rownum || ''',' || rownum || ', ''11.2.0.2'', 8);' from all_source where rownum <= &INST_NUM;
+select 'exec sash_pkg.configure_db(''&DBNAME' || 1 || ''');' from dual;
+select 'exec sash_pkg.set_dbid(''&DBNAME' || 1 || ''');' from dual;
 select 'exec sash_repo.setup_jobs' from dual;
 select 'exec sash_repo.start_collecting_jobs' from dual;
 select 'exec sash_repo.purge_tables' from dual;
--- sash_dev/target_user_view_11g1.sql
+++ sash_dev/target_user_view_11g1.sql
@@ -34,6 +34,7 @@ create user sash identified by &SASH_PASS default tablespace &SASH_TS temporary 
 
 -- sash user grants 
 grant create session to sash;				 
+grant select on sys.sashnow to sash;
 grant select on v_$database to sash;
 grant select on dba_users to sash;
 grant select on v_$sql to sash;
@@ -117,5 +118,4 @@ where
        (  select event# from v$event_name where wait_class='Idle' )
     );
     
-grant select on sys.sashnow to sash;
 exit
