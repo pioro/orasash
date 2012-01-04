128bfeb552adbb71a24bc544f4d427f09de7de80
Marcin Przepiorowski <pioro1@gmail.com>
2012-01-04 08:52:14 +0000

    Revert "Revert "10g fix, same db name fix""
    This reverts commit cc5258071c85b43d95fc8cf69444069ce2c18935.

--- sash_dev/config.sql
+++ sash_dev/config.sql
@@ -44,45 +44,6 @@ prompt  Instalation completed. Starting SASH configuration process
 prompt  Press Control-C if you do not want to configure target database at that time.
 prompt "------------------------------------------------------------------------------------"
 
-
-accept DBNAME prompt "Enter target database name " 
-
-accept INST_NUM default 1 prompt "Enter number of instances [default 1]"
-
-SET TERMOUT OFF
-
-spool instance.sql
-select 'accept HOST' || rownum || ' prompt "Enter host name for instance number ' || rownum || ' "' from all_source where rownum <= &INST_NUM;
-select 'accept INST' || rownum || ' default ' || decode('&INST_NUM',1,'&DBNAME', '&DBNAME' || rownum) || ' prompt "Enter instance name for instance number ' 
-       || rownum || ' [ default ' || decode('&INST_NUM',1,'&DBNAME', '&DBNAME' || rownum) ||' ] "' from all_source where rownum <= &INST_NUM;
-spool off
-
-SET TERMOUT ON
-
-@instance.sql
-
-accept PORT default 1521 prompt "Enter listener port number [default 1521] "
-accept SASHPASS prompt "Enter SASH password on target database "
-
-set term off
-spool setup.sql
-select 'exec sash_repo.add_db(''&' || 'HOST' || rownum || ''', ' || &PORT || ', ''&SASHPASS'', ''&DBNAME'', ''&' || 'INST' || rownum || ''',' || rownum || ', ''11.2.0.2'', 8);' from all_source where rownum <= &INST_NUM;
-select 'exec sash_pkg.configure_db(''&DBNAME' || 1 || ''');' from dual;
-select 'exec sash_pkg.set_dbid(''&DBNAME' || 1 || ''');' from dual;
-select 'exec sash_repo.setup_jobs' from dual;
-select 'exec sash_repo.start_collecting_jobs' from dual;
-select 'exec sash_repo.purge_tables' from dual;
-spool off
-set term on
-
-spool setup.log
-@setup.sql
-
-prompt "------------------------------------------------------------------------------------"
-prompt  Configuration completed. Exiting. 
-prompt  You can now connect using user name and password specified above
-prompt "------------------------------------------------------------------------------------"
-
-spool off
+@adddb.sql
 
 exit 
