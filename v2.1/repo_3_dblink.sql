d286d38f58ac667a3e100d1d1cd251a23c973408
Marcin Przepiorowski <pioro1@gmail.com>
2012-01-04 09:06:07 +0000

    version 2.2

--- v2.1/repo_3_dblink.sql
+++
@@ -1,9 +0,0 @@
-accept SASH_PASS default sash prompt "Enter target database SASH password ? "
-accept TNSALIAS default sash prompt "Enter target database TNS alias ? "
-prompt "Target database tns alias is : " &TNSALIAS
-
-drop database link sashprod;
-create database link sashprod connect to sash identified by &SASH_PASS using '&TNSALIAS';
-
-prompt "Check database link - row from dual table is expected"
-select * from dual@sashprod;
