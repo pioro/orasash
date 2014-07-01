91f64f78e811b9787fbbe962302f79cba36e28a6
Marcin Przepiorowski <pioro1@gmail.com>
2014-07-01 21:58:12 +0100

    missing output variable declaration in switchdb.sql Thanks Ernst

--- sash_dev/switchdb.sql
+++ sash_dev/switchdb.sql
@@ -3,6 +3,14 @@ set pagesize 999
 SET VER OFF
 set feedback off
 
+var output varchar2(2000)
+
+begin
+  :output:='';
+end;
+/
+
+
 col current_db format a10
 select s.dbname, s.dbid, s.host, s.inst_num, case when t.dbid is null then ' ' else '*' end current_db from sash_targets s, sash_target t where s.dbid = t.dbid(+)
 and s.inst_num = t.inst_num(+) order by dbid, inst_num;
